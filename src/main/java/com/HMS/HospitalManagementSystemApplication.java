5433226d4f5b8754a4d487d796e4bd9ddc4b4462
Bhagwat-Patil <bhagwatpatil11@gmail.com>
2025-10-08 18:34:52 +0530

    Database connection added

--- src/main/java/com/HMS/HospitalManagementSystemApplication.java
+++ src/main/java/com/HMS/HospitalManagementSystemApplication.java
@@ -2,12 +2,17 @@ package com.HMS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HospitalManagementSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HospitalManagementSystemApplication.class, args);
+		SpringApplication app= new SpringApplication(HospitalManagementSystemApplication.class);
+
+		ConfigurableApplicationContext context = app.run(args);
+		String localhostLink = "http://localhost:8080";
+		System.out.println("Application is running at : "+localhostLink);
 	}
 
 }
